3244cb3eed6d5b5aabb98d6d5387ab65392e82e4
amkar11 <acmyr97@gmail.com>
2024-11-25 23:09:59 +0100

    Lets go

--- PP-Simulator/Maps/Map.cs
+++ PP-Simulator/Maps/Map.cs
@@ -1,14 +1,26 @@
 /// <summary>
 /// Map of points.
 /// </summary>
-public abstract class Map
+public class Map
 {
+    public int Size { get; }
+    public Map(int size)
+        {
+            if (size < 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "Size must be between 5 and 20.");
+            }
+
+            Size = size;
+        }
     /// <summary>
     /// Check if give point belongs to the map.
     /// </summary>
     /// <param name="p">Point to check.</param>
     /// <returns></returns>
-    public abstract bool Exist(Point p);
+    public virtual bool Exist(Point p){
+        return p.X >= 0 && p.X <= Size - 1 && p.Y >= 0 && p.Y <= Size - 1;
+    }
     
     /// <summary>
     /// Next position to the point in a given direction.
@@ -16,7 +28,13 @@ public abstract class Map
     /// <param name="p">Starting point.</param>
     /// <param name="d">Direction.</param>
     /// <returns>Next point.</returns>
-    public abstract Point Next(Point p, Direction d);
+    public virtual Point Next(Point p, Direction d){
+        if (Exist(p)){
+                p = p.Next(d);
+                if (p.X > Size - 1 || p.X < 0 || p.Y > Size - 1 || p.Y < 0) {throw new ArgumentOutOfRangeException("Jedna z wartości przekroczyła Size lub stała mniejsza od zera");}
+            }
+            return p;
+    }
 
     /// <summary>
     /// Next diagonal position to the point in a given direction 
@@ -25,5 +43,11 @@ public abstract class Map
     /// <param name="p">Starting point.</param>
     /// <param name="d">Direction.</param>
     /// <returns>Next point.</returns>
-    public abstract Point NextDiagonal(Point p, Direction d);
+    public virtual Point NextDiagonal(Point p, Direction d){
+        if (Exist(p)){
+                p = p.NextDiagonal(d);
+                if (p.X > Size - 1 || p.X < 0 || p.Y > Size - 1 || p.Y < 0) {throw new ArgumentOutOfRangeException("Jedna z wartości przekroczyła Size lub stała mniejsza od zera");}
+            }
+            return p;
+    }
 }
